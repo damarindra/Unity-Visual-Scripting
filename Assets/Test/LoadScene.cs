5d2956561ca50fbc0250aedde7b5ad1022321410
Damar Inderajati <damarind@gmail.com>
2018-12-03 22:07:26 +0700

    backup

---
+++ Assets/Test/LoadScene.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoadScene : MonoBehaviour {
+
+	// Use this for initialization
+	IEnumerator Start () {
+		yield return new WaitForSeconds(.6f);
+		UnityEngine.SceneManagement.SceneManager.LoadScene("2");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
